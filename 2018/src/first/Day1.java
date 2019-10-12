c3359eb1c021d74173d4fb6135162987ad1f9e80
Kristof Achten <kristofachten@gmail.com>
2019-10-12 15:21:28 +0200

    Update of the structure

--- 2018/src/first/Day1.java
+++ 2018/src/first/Day1.java
@@ -1,21 +1,19 @@
 package first;
 
-import javax.print.attribute.IntegerSyntax;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class main {
-    public static void main(String[] args) throws FileNotFoundException {
+public class Day1 {
+    public static void main(String[] args) {
         Supplier<IntStream> supplier = () -> {
             try {
-                return new BufferedReader(new FileReader(new File("src/first/input.txt"))).lines().mapToInt(Integer::parseInt);
+                return new BufferedReader(new FileReader(new File("2018/src/first/input.txt"))).lines().mapToInt(Integer::parseInt);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
