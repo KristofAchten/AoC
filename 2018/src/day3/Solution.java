4a0dbc2eb76a8bead1eff50f04d369cc414b427c
Kristof Achten <kristofachten@gmail.com>
2019-10-15 23:55:58 +0200

    Day 3: second part completed

--- 2018/src/day3/Solution.java
+++ 2018/src/day3/Solution.java
@@ -2,8 +2,10 @@ package day3;
 
 import common.Tools;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,10 +16,38 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println("Overlap (in^2): " + calculateOverlap(Tools.getInputSupplier(PATH).get()));
+        final Map.Entry resultPair = calculateOverlap(Tools.getInputSupplier(PATH).get());
+
+        System.out.println("Overlap (in^2): " + resultPair.getKey());
+        System.out.println("Unique pattern: " + determineUniquePattern(Tools.getInputSupplier(PATH).get(),
+                (States[][]) resultPair.getValue()));
     }
 
-    private static int calculateOverlap(Stream<String> input) {
+    private static String determineUniquePattern(Stream<String> input, States[][] fabric) {
+        ArrayList<String> claims = (ArrayList<String>) input.collect(Collectors.toList());
+
+        for (String c : claims) {
+            boolean unique = true;
+            int[] usefulValues = Arrays.stream(parseEntry(c)).mapToInt(Integer::parseInt).toArray();
+            for (int x = usefulValues[0]; x < usefulValues[0] + usefulValues[2]; x++) {
+                for (int y = usefulValues[1]; y < usefulValues[1] + usefulValues[3]; y++) {
+                    if (fabric[x][y] == States.TWICE_OR_MORE) {
+                        unique = false;
+                        break;
+                    }
+                }
+                if (!unique) {
+                    break;
+                }
+            }
+            if (unique) {
+                return c.split(" ")[0];
+            }
+        }
+        return "Not found";
+    }
+
+    private static Map.Entry calculateOverlap(Stream<String> input) {
         final States[][] fabric = new States[1000][1000];
         for(int i = 0; i < fabric.length; i++) {
             for(int j = 0; j < fabric[0].length; j++) {
@@ -30,12 +60,8 @@ public class Solution {
         ArrayList<String> claims = (ArrayList<String>) input.collect(Collectors.toList());
 
         for(String c : claims) {
-            String[] usefulParts = Arrays.copyOfRange(c.replaceAll(":", "")
-                    .replaceAll("x", " ")
-                    .replaceAll(",", " ")
-                    .split(" "), 2, 6);
 
-            int[] usefulValues = Arrays.stream(usefulParts).mapToInt(Integer::parseInt).toArray();
+            int[] usefulValues = Arrays.stream(parseEntry(c)).mapToInt(Integer::parseInt).toArray();
 
             for (int x = usefulValues[0]; x < usefulValues[0] + usefulValues[2]; x++) {
                 for (int y = usefulValues[1]; y < usefulValues[1] + usefulValues[3]; y++) {
@@ -53,6 +79,14 @@ public class Solution {
                 }
             }
         }
-        return sqInches;
+        return new AbstractMap.SimpleEntry<>(sqInches, fabric);
+    }
+
+    private static String[] parseEntry(String input) {
+        return Arrays.copyOfRange(input
+                .replaceAll(":", "")
+                .replaceAll("x", " ")
+                .replaceAll(",", " ")
+                .split(" "), 2, 6);
     }
 }
